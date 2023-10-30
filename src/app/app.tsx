fca927fa3fd3ab9c44e77568aa947a4ce9b767f3
Damian <ruiz.damian17@gmail.com>
2023-10-30 16:20:26 -0400

    Refactor Components into App

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,5 +1,6 @@
+import TodoInput from 'src/components/TodoInput/TodoInput'
+import TodoListComponent from 'src/components/TodoListComponent/TodoListComponent'
 import { DEFAULT_TODOS } from '../constants'
-import { Todo, TodoList } from '../types'
 
 export function App() {
   return (
@@ -8,51 +9,6 @@ export function App() {
       <TodoInput />
     </>
   )
-}
-
-interface TodoListProps {
-  todos: TodoList
-}
-
-function TodoListComponent({ todos }: TodoListProps) {
-  const todoElements = todos.map((todo) => (
-    <TodoItem
-      title={todo.title}
-      description={todo.description}
-      completed={todo.completed}
-      due={todo.due}
-    />
-  ))
-  return (
-    <div>
-      List
-      {todoElements}
-    </div>
-  )
-}
-
-interface TodoItemProps extends Todo {}
-
-function TodoItem({ title, description, completed, due }: TodoItemProps) {
-  return (
-    <div>
-      <h2>{title}</h2>
-      <p>{description}</p>
-      <p>Status: {completed ? 'Completed' : 'Not Completed'}</p>
-      <p>
-        Due:{' '}
-        {due
-          ? `${due.getMonth() + 1}/${due.getDate()}/${due.getFullYear()}`
-          : 'Whenever'}
-      </p>
-    </div>
-  )
-}
-
-interface TodoInputProps {}
-
-function TodoInput({}: TodoInputProps) {
-  return <div>Input</div>
 }
 
 export default App
