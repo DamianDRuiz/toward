ba59ef44fb21bd2b3d82ae4586da074e08777ee3
Damian <ruiz.damian17@gmail.com>
2023-10-30 17:49:21 -0400

    App root styling

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,6 +2,7 @@ import TodoInput from 'src/components/TodoInput/TodoInput'
 import TodoListComponent from 'src/components/TodoListComponent/TodoListComponent'
 import useTodos from 'src/hooks/useTodos/useTodos'
 import { DEFAULT_TODOS } from 'src/util/constants'
+import styles from './app.module.scss'
 
 export function App() {
   const {
@@ -12,14 +13,14 @@ export function App() {
     handleAddTodoSubmit,
   } = useTodos(DEFAULT_TODOS)
   return (
-    <>
+    <div className={styles.toward}>
       <TodoListComponent todos={todos} handleTodoClick={handleTodoClick} />
       <TodoInput
         input={addTodoInput}
         onChange={handleAddTodoInput}
         onSubmit={handleAddTodoSubmit}
       />
-    </>
+    </div>
   )
 }
 
